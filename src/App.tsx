628e33cac847c745423da7726824c831bf666547
tormknd <chhajuck@gmail.com>
2025-06-10 16:29:11 +0200

    add online server url

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export default function App() {
     formData.append("stage", stage);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/send", {
+      const response = await fetch("https://mailsenderapi-mmeo.onrender.com/send", {
         method: "POST",
         body: formData,
       });
